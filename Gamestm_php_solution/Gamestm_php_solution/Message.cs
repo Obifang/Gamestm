56e01052637c70ac6f8f9f7392b43c6bc4ea4910
Thomas <9718648@student.swin.edu.au>
2017-04-05 20:29:30 +1000

    base of program part 1
    I have made a rough simulation of what we might want out of thee input
    and output of the program

---
+++ Gamestm_php_solution/Gamestm_php_solution/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Gamestm_php_solution
+{
+	public class Message
+	{
+		private string text;
+		public Message(string txt)
+		{
+			text = txt;
+		}
+		public void Print()
+		{
+			Console.WriteLine(text);
+		}
+	}
+}
--- Gamestm_php_solution/Gamestm_php_solution/Program.cs
+++ Gamestm_php_solution/Gamestm_php_solution/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
-
+//A Hello World! program in C#.
 namespace Gamestm_php_solution
 {
 	class MainClass
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Message myMessage = new Message("This is the PHP_Inc Program:");
+			myMessage.Print();
+
+			Message[] messages = new Message[4];
+			messages[0] = new Message(" Hello World ");
+			messages[1] = new Message(" My name is Tom Latimer");
+			messages[2] = new Message(" Nice to meet you ");
+			messages[3] = new Message(" This is my first time doing object oriented programming ");
+
+			Console.WriteLine("Insert Item Name:");
+			string name = Console.ReadLine();
+			//MITCH = mitch
+			if (name.ToLower() == "andrew")
+			{
+				messages[0].Print();
+			}
+			else if (name.ToLower() == "wilma")
+			{
+				messages[1].Print();
+			}
+			else if (name.ToLower() == "fred")
+			{
+				messages[2].Print();
+			}
+			else
+			{
+				messages[3].Print();
+			}
 		}
 	}
 }
+/*int num;
+			Console.WriteLine("Enter a number:");
+			num = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("you have inserted a number");
+			Console.ReadLine();*/
