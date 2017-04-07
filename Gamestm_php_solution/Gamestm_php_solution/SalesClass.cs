719d71842bf13d5094956e158e7d6210f143c9d5
obifang <obifang@gmail.com>
2017-04-07 11:44:34 +1000

    Test Commit

--- Gamestm_php_solution/Gamestm_php_solution/SalesClass.cs
+++ Gamestm_php_solution/Gamestm_php_solution/SalesClass.cs
@@ -5,8 +5,8 @@ namespace Gamestm_php_solution
 {
 	public class SalesClass
 	{
-		private StockClass[] _StockItems;
-		private int _StockCount;
+		//private StockClass[] _StockItems;
+		//private int _StockCount;
 
 		public SalesClass ()
 		{
