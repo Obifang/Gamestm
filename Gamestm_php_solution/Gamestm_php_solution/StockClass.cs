29c05b3e77e2a1ae71d699989d8fb291cf852a26
obifang <obifang@gmail.com>
2017-04-07 11:18:16 +1000

    Updated changes from previous commit

--- Gamestm_php_solution/Gamestm_php_solution/StockClass.cs
+++ Gamestm_php_solution/Gamestm_php_solution/StockClass.cs
@@ -4,8 +4,44 @@ namespace Gamestm_php_solution
 {
 	public class StockClass
 	{
-		public StockClass ()
+		private string _Name;
+		private string _Desc;
+		private int _Id;
+		private double _Price;
+
+		public StockClass (string Name, string Desc, int Id, double Price)
 		{
+			_Name = Name;
+			_Desc = Desc;
+			_Id = Id;
+			_Price = Price;
+		}
+
+		public string GetName {
+			get {
+				return _Name;
+			}
+		}
+
+		public double Price {
+			get {
+				return _Price;
+			}
+			set {
+				_Price = value;
+			}
+		}
+
+		public int GetId {
+			get {
+				return _Id;
+			}
+		}
+
+		public string GetDesc {
+			get {
+				return _Desc;
+			}
 		}
 	}
 }
