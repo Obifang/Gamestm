02c2f2de88d3a453bfeabeba4d678af7935a927e
RubyDevlin <1005090954@student.swin.edu.au>
2017-04-17 15:31:17 +1000

    * Spaced out testing, edit now has 1 test for each field

--- Gamestm_php_solution/Gamestm_php_solutionTests/StockClassTests.cs
+++ Gamestm_php_solution/Gamestm_php_solutionTests/StockClassTests.cs
@@ -12,19 +12,47 @@ namespace Gamestm_php_solution.Tests
     public class StockClassTests
     {
         [TestMethod()]
-        public void editTest()
+        public void editNameTest()
         {
             StockClass testStock = new StockClass("stock name", "stock desc", 1, 2, 3);
             Assert.AreEqual(testStock.GetName, "stock name");
-            Assert.AreEqual(testStock.GetDesc, "stock desc");
-            Assert.AreEqual(testStock.GetId, 1);
-            Assert.AreEqual(testStock.GetPrice, 2);
 
             testStock.edit("new name", "new desc", 4, 5);
             Assert.AreEqual(testStock.GetName, "new name");
+
+            //Assert.Fail();
+        }
+        [TestMethod()]
+        public void editDescTest()
+        {
+            StockClass testStock = new StockClass("stock name", "stock desc", 1, 2, 3);
+            Assert.AreEqual(testStock.GetDesc, "stock desc");
+
+            testStock.edit("new name", "new desc", 4, 5);
             Assert.AreEqual(testStock.GetDesc, "new desc");
+
+            //Assert.Fail();
+        }
+        [TestMethod()]
+        public void editIdTest()
+        {
+            StockClass testStock = new StockClass("stock name", "stock desc", 1, 2, 3);
+            Assert.AreEqual(testStock.GetId, 1);
+
+            testStock.edit("new name", "new desc", 4, 5);
             Assert.AreEqual(testStock.GetId, 4);
+
+            //Assert.Fail();
+        }
+        [TestMethod()]
+        public void editPriceTest()
+        {
+            StockClass testStock = new StockClass("stock name", "stock desc", 1, 2, 3);
+            Assert.AreEqual(testStock.GetPrice, 2);
+
+            testStock.edit("new name", "new desc", 4, 5);
             Assert.AreEqual(testStock.GetPrice, 5);
+
             //Assert.Fail();
         }
     }
