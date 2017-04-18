430691d8bfaa6a5064d5d35434f54e20678cc2b6
TomLatimer92 <9718648@student.swin.edu.au>
2017-04-18 18:36:35 +1000

    small tweak

--- Gamestm_php_solution/Gamestm_php_solution/StockClass.cs
+++ Gamestm_php_solution/Gamestm_php_solution/StockClass.cs
@@ -15,7 +15,7 @@ namespace Gamestm_php_solution
         //cost of item.
 		private double _Price;
         //find out what stock is available.
-        private int _InStock:
+        private int _InStock;
         //maybe have "Quantity" as a private variable if need be.
 		public StockClass (string Name, string Desc, int Id, double Price)
 		{
@@ -67,7 +67,7 @@ namespace Gamestm_php_solution
 		}
 	}
 }
-
+/*
         //get and find out if there is stock available or not.
         public int InStock
         {
@@ -84,4 +84,4 @@ namespace Gamestm_php_solution
             //just need to know what to insert here.
         }
     }
-}
+}*/
