2f12b56e1f98dce1e1ea575f28ad2cc871635946
TomLatimer92 <9718648@student.swin.edu.au>
2017-04-18 18:13:47 +1000

    update 3 p5

--- Gamestm_php_solution/Gamestm_php_solution/StockSale.cs
+++ Gamestm_php_solution/Gamestm_php_solution/StockSale.cs
@@ -1,5 +1,8 @@
 ﻿using System;
-//using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Gamestm_php_solution
 {
@@ -9,28 +12,37 @@ namespace Gamestm_php_solution
         private int _StockSold;
 
         //set values and private variables.
-	    public StockSale(String StockName, int StockSold)
-	    {
-	        _StockName = StockName;
-	        _StockSold = StockSold;
-	    }
+        public StockSale(String StockName, int StockSold)
+        {
+            _StockName = StockName;
+            _StockSold = StockSold;
+        }
 
-	    //Get the name of the stock.	
-	    public string GetStockName
-	    {
-	        get
-	        {
-	            return _StockName;
-	        }
-	    }
+        //Get the name of the stock.	
+        public string GetStockName
+        {
+            get
+            {
+                return _StockName;
+            }
+        }
 
-	    //Get the amount of stock sold.	
-		public int GetStockSold
-	    {
-	        get
-	        {
-	            return _StockSold;
-	        }
-	    }
-	}
+        //Get the amount of stock sold.	
+        public int GetStockSold
+        {
+            get
+            {
+                return _StockSold;
+            }
+        }
+
+        //Get the amount of stock sold.	
+        public int UpdateStockSold
+        {
+            set
+            {
+                _StockSold -= value;
+            }
+        }
+    }
 }
