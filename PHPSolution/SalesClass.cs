f91d2cc927cbcf3f4ab3b7969e5a9911e01b54a2
TomLatimer92 <9718648@student.swin.edu.au>
2017-04-20 13:01:38 +1000

    another small tweak
    I hope some of this code is useful. get rid of it if it isn't

--- PHPSolution/SalesClass.cs
+++ PHPSolution/SalesClass.cs
@@ -8,12 +8,46 @@ namespace PHPSolution
 {
     class SalesClass
     {
-        //private StockClass[] _StockItems;
-        //private int _StockCount;
+        //Stock Items.
+        private StockClass[] _StockItems;
+        //Quantity of stock.
+        private int _StockCount;
+        //Date stock item was sold.
+        private DateTime _SalesDate;
 
         public SalesClass()
         {
-           //?.
+            //?.
+            _StockItems = null;
+            _SalesDate = default(DateTime);
+            _StockCount = 0;
+        }
+        //bring up stock data in set messages.
+public StockClass[] StockItems
+        {
+            get
+            {
+                return _StockItems;
+            }
+        }
+
+        //with each stock added - increase quantity of item.
+        //calculate the amount of purchases of each sales item.
+        public int StockCount
+        {
+            get
+            {
+                return _StockCount;
+            }
+        }
+
+        //enable the sales date to synchronise with the clock on the computer.
+        public DateTime SalesDate
+        {
+            get
+            {
+                return _SalesDate;
+            }
         }
     }
 }
