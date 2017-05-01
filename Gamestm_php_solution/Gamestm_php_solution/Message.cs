11570ded7d1d954a4e0c1a2e093637a8b1698ed1
TomLatimer92 <9718648@student.swin.edu.au>
2017-05-01 16:06:31 +1000

    change
    change

---
+++ Gamestm_php_solution/Gamestm_php_solution/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+//using System.Collections.Generic;
+
+//print out text and make sure it prints out.
+namespace Gamestm_php_solution
+{
+	public class Message
+	{
+		private string text;
+
+		public Message(string txt)
+		{
+			text = txt;
+		}
+
+        //once the text inserted is given a value and then printed out.
+		public void Print()
+		{
+			Console.WriteLine(text);
+		}
+	}
+}
---
+++ Gamestm_php_solution/Gamestm_php_solution/StockSale.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gamestm_php_solution
+{
+    public class StockSale
+    {
+        private string _StockName;
+        private int _StockSold;
+
+        //set values and private variables.
+        public StockSale(String StockName, int StockSold)
+        {
+            _StockName = StockName;
+            _StockSold = StockSold;
+        }
+
+        //Get the name of the stock.	
+        public string GetStockName
+        {
+            get
+            {
+                return _StockName;
+            }
+        }
+
+        //Get the amount of stock sold.	
+        public int GetStockSold
+        {
+            get
+            {
+                return _StockSold;
+            }
+        }
+
+        //Get the amount of stock sold.	
+        public int UpdateStockSold
+        {
+            set
+            {
+                _StockSold -= value;
+            }
+        }
+    }
+}
