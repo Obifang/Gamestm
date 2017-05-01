570534438144c96045f0f1f632303f06fe6d67f3
TomLatimer92 <9718648@student.swin.edu.au>
2017-05-01 12:51:12 +1000

    tweak merge
    tweaked so it can merge with master

---
+++ PHPSolution/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//print out text and make sure it prints out.
+namespace PHPSolution
+{
+    public class Message
+    {
+        private string text;
+
+        public Message(string txt)
+        {
+            text = txt;
+        }
+
+        //once the text inserted is given a value and then printed out.
+        public void Print()
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
